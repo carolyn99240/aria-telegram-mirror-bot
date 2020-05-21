d245a5ae209b0b8d1fce221c44d1b7e8d064415d
carolyn99240 <59831525+carolyn99240@users.noreply.github.com>
2020-05-21 10:58:31 +0530

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,12 +1,12 @@
 module.exports = Object.freeze({
-  TOKEN: '1282925772:AAEk4yxRGuIXBFBKQxx176i0Xa8yTcjWpW4',
+  TOKEN: '964258004:AAFtVxDZW6msgnehf5vy7DHhND4YxOlBHpE',
   ARIA_SECRET: 'ARIA_RPC_SECRET',
   ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
   ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: ['YIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  GDRIVE_PARENT_DIR_ID: '18wkK4Lzk8OtU9Q3JC0-GYM3Ys-fHLkmB',
+  GDRIVE_PARENT_DIR_ID: '1VrbnA4GLp3YzoPsXj0QPZoyNsmaRz5Ps',
   SUDO_USERS: [012, 345],	// Telegram user IDs. These users can use the bot in any chat.
   AUTHORIZED_CHATS: [678, 901],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
@@ -24,5 +24,5 @@ module.exports = Object.freeze({
     ENABLED: false,  // If true, all commands except '/list' has to have the bot username after the command
     NAME: "@SledgerCloud03bot"
   },
-  IS_TEAM_DRIVE: false
+  IS_TEAM_DRIVE: true
 });
